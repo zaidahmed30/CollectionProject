79c1129741bece241e3294bdbb9d951ad42a5ce9
Zaid Ahmed <ahmedzaid219@outlook.com>
2025-11-26 15:48:37 +0530

    Add project files.

---
+++ CollectionProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //int[] arr = new int[10];
+        //Array.Resize(ref arr, 15);//It destroy the old array and create a new one with new size
+        ArrayList al = new ArrayList();
+        Console.WriteLine(al.Capacity);
+        al.Add(100);
+        Console.WriteLine(al.Capacity);
+        al.Add(200); al.Add(300); al.Add(400);
+        Console.WriteLine(al.Capacity);
+        al.Add(500);
+        Console.WriteLine(al.Capacity);
+        foreach (object obj in al)
+            Console.Write(obj + " ");
+        Console.WriteLine();
+        al.Insert(3, 350);
+        foreach (object obj in al)
+            Console.Write(obj + " ");
+        Console.WriteLine();
+        //al.Remove(200);
+        al.RemoveAt(1);
+        foreach (object obj in al)
+            Console.Write(obj + " ");
+        Console.WriteLine();
+    }
+}
